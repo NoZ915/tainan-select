59ffb0abfca7796a0a83abdf64bcb2089db616cc
NoZ915 <tina0915tw0915@gmail.com>
2025-02-27 08:19:05 +0800

    fix:Avoiding JSON hijacking
    如果回傳的東西是陣列，可能會遭到JSON hijacking來取得網頁回傳資料
    可參考：為何 Facebook API 要回傳無窮迴圈？ 談捲土重來的 JSON Hijacking

--- backend/controllers/courseController.ts
+++ backend/controllers/courseController.ts
@@ -4,7 +4,7 @@ import { Request, RequestHandler, Response } from "express";
 export const getAllCourses: RequestHandler = async (req, res): Promise<void> => {
   try {
     const courses = await CourseService.getAllCourses();
-    res.status(200).json(courses);
+    res.status(200).json({ courses });
   } catch (err) {
     res.status(500).json({ message: err });
   }
