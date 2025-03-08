b5e8282d0dea04bb1a28975ca88463522d800c6a
NoZ915 <tina0915tw0915@gmail.com>
2025-03-08 14:38:08 +0800

    update:Some setting

--- frontend/apis/axiosInstance.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import "dotenv/config";
-
-export const axiosInstance = axios.create({
-  baseURL: process.env.VITE_API_BASE_URL
-});
-console.log(process.env.VITE_API_BASE_URL);
