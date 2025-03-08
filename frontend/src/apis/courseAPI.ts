cdc67ae6c46a4de4b06df2f3b0884710f2990a5c
NoZ915 <tina0915tw0915@gmail.com>
2025-03-09 00:34:36 +0800

    fix:API route from frontend

--- frontend/src/apis/courseAPI.ts
+++ frontend/src/apis/courseAPI.ts
@@ -1,8 +1,8 @@
 import { CourseResponse } from '../types/courseType';
 import { axiosInstance } from './axiosInstance';
 
-export const getCourses = async({ page = 1, limit = 10 }): Promise<CourseResponse> => {
-    const response = await axiosInstance.get("/api/courses", {
+export const getCourses = async ({ page = 1, limit = 10 }): Promise<CourseResponse> => {
+    const response = await axiosInstance.get("/courses", {
         params: { page, limit }
     });
     console.log(response.data)
