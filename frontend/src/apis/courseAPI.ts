99a7f613b5b4982ca3afc6ecf56adb406285163a
NoZ915 <tina0915tw0915@gmail.com>
2025-03-16 14:20:22 +0800

    fix:Type issue

--- frontend/src/apis/courseAPI.ts
+++ frontend/src/apis/courseAPI.ts
@@ -20,7 +20,7 @@ export const getCourses = async ({
   return response.data;
 };
 
-export const getCourse = async (course_id: string): Promise<Course | null> => {
+export const getCourse = async (course_id: string): Promise<{course: Course} | null> => {
   const response = await axiosInstance.get(`/courses/${course_id}`);
   return response.data;
 }
--- frontend/src/components/CourseInfoPanel.tsx
+++ frontend/src/components/CourseInfoPanel.tsx
@@ -1,7 +1,7 @@
 import { Course } from "../types/courseType";
 
 interface CourseInfoPanelProps {
-  course: Course | null | undefined;
+  course: { course: Course } | null | undefined;
   isLoading: boolean;
 }
 
@@ -14,11 +14,11 @@ const CourseInfoPanel: React.FC<CourseInfoPanelProps> = ({ course, isLoading }) 
   if (!course) {
     return <>Course information is not available.</>;
   }
-  
+
   return (
     <>
-    <h2>{course.course_name}</h2>
-    <p>{course.academy}</p>
+    <h2>{course.course.course_name}</h2>
+    <p>{course.course.academy}</p>
   </>
   )
 }
