3450d5bb86f77d0b4ffecd4e51cb1bd5f6c48904
NoZ915 <tina0915tw0915@gmail.com>
2025-03-16 12:58:49 +0800

    feat:Add two columns with migrations

---
+++ backend/migrations/20250316045412-add-request_count-and-view_count-into-courses-table.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Courses', 'request_count', {
+      type: Sequelize.INTEGER,
+      allowNull: false, 
+      defaultValue: 0, 
+    });
+
+    await queryInterface.addColumn('Courses', 'view_count', {
+      type: Sequelize.INTEGER, 
+      allowNull: false,
+      defaultValue: 0,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Courses', 'request_count');
+    await queryInterface.removeColumn('Courses', 'view_count');
+  }
+};
