86c94298a7adba63aeba292c52622a15345d3f02
NoZ915 <tina0915tw0915@gmail.com>
2025-03-30 14:53:04 +0800

    feat:Add reviews to frontend

--- frontend/src/components/CourseReviewsPanel.tsx
+++ frontend/src/components/CourseReviewsPanel.tsx
@@ -1,12 +1,65 @@
-import { Card, Flex } from "@mantine/core";
-import style from "../styles/components/CourseReviewsPanel.module.css";
+import { Avatar, Box, Card, Container, Group, Rating, Text } from "@mantine/core";
+import styles from "../styles/components/CourseReviewsPanel.module.css";
+import { Review } from "../types/reviewType";
+import { Course } from "../types/courseType";
 
-const CourseReviewsPanel: React.FC = () => {
-    return (
-        <Flex direction="column" align="flex-start" className={style.flex}>
-            <Card></Card>
-        </Flex>
-    )
+interface CourseReviewsPanelProps {
+	course: { course: Course } | null | undefined;
+	reviews: Review[] | undefined;
+	isLoading: boolean;
+}
+
+const CourseReviewsPanel: React.FC<CourseReviewsPanelProps> = ({ course, reviews, isLoading }) => {
+	if (isLoading) {
+		return <>Is Loading...</>
+	}
+
+	if (!reviews) {
+		return <>There are no reviews.</>;
+	}
+
+	return (
+		<Container className={styles.container}>
+			{reviews.map((review) => {
+				return (
+					<Card key={review.id} className={styles.card}>
+						<Card.Section className={styles.cardSection}>
+							<Group>
+								<Avatar variant="light" size="lg" color="brick-red.6" src="" />
+								<Box>
+									<Text>{review.UserModel.name}</Text>
+									<Text>{new Date(review.updated_at).toLocaleString()}</Text>
+								</Box>
+							</Group>
+						</Card.Section>
+
+						<Card.Section className={styles.cardSection}>
+							<Text size="md" fw={900} className={styles.courseName}>
+								{course?.course.course_name} / {course?.course.instructor}
+							</Text>
+						</Card.Section>
+
+						<Card.Section className={styles.cardSection}>
+							<Group>
+								<Text>收穫</Text>
+								<Rating defaultValue={review.gain} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
+
+								<Text>甜度</Text>
+								<Rating defaultValue={review.sweetness} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
+
+								<Text>涼度</Text>
+								<Rating defaultValue={review.coolness} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
+							</Group>
+						</Card.Section>
+
+						<Card.Section className={styles.cardSection}>
+							<Text>{review.comment}</Text>
+						</Card.Section>
+					</Card>
+				)
+			})}
+		</Container>
+	)
 }
 
 export default CourseReviewsPanel;
--- frontend/src/pages/CourseDetailPage.tsx
+++ frontend/src/pages/CourseDetailPage.tsx
@@ -4,20 +4,22 @@ import CourseReviewsPanel from "../components/CourseReviewsPanel";
 import { useGetCourse } from "../hooks/courses/useGetCourse";
 import { useParams } from "react-router-dom";
 import { useIsMobile } from "../hooks/useIsMobile";
+import { useGetAllReviewsByCourseId } from "../hooks/reviews/useGetAllReviewsByCourseId";
 
 const CourseDetailPage: React.FC = () => {
   const isMobile = useIsMobile();
   const { course_id } = useParams<{ course_id: string }>();
   const { data: course, isLoading: isInfoLoading } = useGetCourse(course_id || '');
+  const { data: reviews, isLoading: isReviewsLoading } = useGetAllReviewsByCourseId(course_id || '');
 
   if (isMobile) {
     return (
       <Container size="lg" mt="lg" style={{  gap: '1rem' }}>
-        <div style={{ flex: '2' }}>
+        <div>
           <CourseInfoPanel course={course} isLoading={isInfoLoading} />
         </div>
-        <div style={{ flex: '3' }}>
-          <CourseReviewsPanel />
+        <div style={{ marginTop: "2rem" }}>
+          <CourseReviewsPanel course={course} reviews={reviews} isLoading={isReviewsLoading}/>
         </div>
       </Container>
     )
@@ -25,11 +27,11 @@ const CourseDetailPage: React.FC = () => {
 
   return (
     <Container size="lg" mt="lg" style={{ display: 'flex', gap: '1rem' }}>
-      <div style={{ flex: '2' }}>
+      <div style={{ flex: '1' }}>
         <CourseInfoPanel course={course} isLoading={isInfoLoading} />
       </div>
-      <div style={{ flex: '3' }}>
-        <CourseReviewsPanel />
+      <div style={{ flex: '2' }}>
+        <CourseReviewsPanel course={course} reviews={reviews} isLoading={isReviewsLoading} />
       </div>
     </Container>
   )
