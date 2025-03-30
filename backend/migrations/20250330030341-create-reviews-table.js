0c6a26ec667b7ab2c21c5254eb970247e29e3c22
NoZ915 <tina0915tw0915@gmail.com>
2025-03-30 11:33:42 +0800

    feat:Add migration for adding reviews table

---
+++ backend/migrations/20250330030341-create-reviews-table.js
@@ -0,0 +1,78 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Reviews", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      course_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Courses",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      gain: {
+        type: Sequelize.DECIMAL(3, 1),
+        allowNull: false,
+        validate: {
+          min: 1.0,
+          max: 5.0,
+        },
+      },
+      sweetness: {
+        type: Sequelize.DECIMAL(3, 1),
+        allowNull: false,
+        validate: {
+          min: 1.0,
+          max: 5.0,
+        },
+      },
+      coolness: {
+        type: Sequelize.DECIMAL(3, 1),
+        allowNull: false,
+        validate: {
+          min: 1.0,
+          max: 5.0,
+        },
+      },
+      comment: {
+        type: Sequelize.TEXT,
+      },
+      favorites: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("Reviews");
+  },
+};
