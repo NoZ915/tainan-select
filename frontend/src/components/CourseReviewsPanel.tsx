4f8e2660218f975a6ebe5abd3f42c7e0a1eb4eea
NoZ915 <tina0915tw0915@gmail.com>
2025-04-06 11:37:07 +0800

    feat:Add edit & delete icon for review

--- frontend/src/components/CourseReviewsPanel.tsx
+++ frontend/src/components/CourseReviewsPanel.tsx
@@ -1,81 +1,95 @@
 import { ActionIcon, Avatar, Box, Card, Container, Group, Menu, Rating, Text } from "@mantine/core";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsThreeDots } from "react-icons/bs";
+import { FaEdit } from "react-icons/fa";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 import styles from "../styles/components/CourseReviewsPanel.module.css";
 import { Review } from "../types/reviewType";
 import { Course } from "../types/courseType";
 
 interface CourseReviewsPanelProps {
-	course: { course: Course } | null | undefined;
-	reviews: Review[] | undefined;
-	isLoading: boolean;
+  course: { course: Course } | null | undefined;
+  reviews: Review[] | undefined;
+  isLoading: boolean;
 }
 
 const CourseReviewsPanel: React.FC<CourseReviewsPanelProps> = ({ course, reviews, isLoading }) => {
-	if (isLoading) {
-		return <>Is Loading...</>
-	}
+  if (isLoading) {
+    return <>Is Loading...</>
+  }
 
-	if (!reviews) {
-		return <>There are no reviews.</>;
-	}
+  if (!reviews) {
+    return <>There are no reviews.</>;
+  }
 
-	return (
-		<Container className={styles.container}>
-			{reviews.map((review) => {
-				return (
-					<Card key={review.id} className={styles.card}>
-						<Card.Section className={styles.cardSection}>
-							<Group justify="space-between">
-								<Group>
-									<Avatar variant="light" size="lg" color="brick-red.6" src="" />
-									<Box>
-										<Text>{review.UserModel.name}</Text>
-										<Text>{new Date(review.updated_at).toLocaleString()}</Text>
-									</Box>
-								</Group>
+  return (
+    <Container className={styles.container}>
+      {reviews.map((review) => {
+        return (
+          <Card key={review.id} className={styles.card}>
+            <Card.Section className={styles.cardSection}>
+              <Group justify="space-between">
+                <Group>
+                  <Avatar variant="light" size="lg" color="brick-red.6" src="" />
+                  <Box>
+                    <Text>{review.UserModel.name}</Text>
+                    <Text>{new Date(review.updated_at).toLocaleString()}</Text>
+                  </Box>
+                </Group>
 
-								<Menu>
-									<Menu.Target>
-										<ActionIcon variant="outline" size="lg" radius="xs">
-											<BsThreeDotsVertical size={16}/>
-										</ActionIcon>
-									</Menu.Target>
+                <Menu>
+                  <Menu.Target>
+                    <ActionIcon variant="fullfill" size="lg" radius="xs">
+                      <BsThreeDots size={16} />
+                    </ActionIcon>
+                  </Menu.Target>
 
-									<Menu.Dropdown>
-										<Menu.Item>編輯</Menu.Item>
-									</Menu.Dropdown>
-								</Menu>
-							</Group>
+                  <Menu.Dropdown className={styles.dropdown}>
+                    <Menu.Item
+                      leftSection={<FaEdit size={16} />}
+                      classNames={{ itemLabel: styles.itemLabel }}
+                    >
+                      編輯
+                    </Menu.Item>
+                    <Menu.Item
+                      leftSection={<RiDeleteBin6Fill size={16} />}
+                      classNames={{ itemLabel: styles.itemLabel }} 
+                      color="red" 
+                    >
+                      刪除
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              </Group>
 
-						</Card.Section>
+            </Card.Section>
 
-						<Card.Section className={styles.cardSection}>
-							<Text size="md" fw={900} className={styles.courseName}>
-								{course?.course.course_name} / {course?.course.instructor}
-							</Text>
-						</Card.Section>
+            <Card.Section className={styles.cardSection}>
+              <Text size="md" fw={900} className={styles.courseName}>
+                {course?.course.course_name} / {course?.course.instructor}
+              </Text>
+            </Card.Section>
 
-						<Card.Section className={styles.cardSection}>
-							<Group>
-								<Text>收穫</Text>
-								<Rating defaultValue={review.gain} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
+            <Card.Section className={styles.cardSection}>
+              <Group>
+                <Text>收穫</Text>
+                <Rating defaultValue={review.gain} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
 
-								<Text>甜度</Text>
-								<Rating defaultValue={review.sweetness} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
+                <Text>甜度</Text>
+                <Rating defaultValue={review.sweetness} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
 
-								<Text>涼度</Text>
-								<Rating defaultValue={review.coolness} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
-							</Group>
-						</Card.Section>
+                <Text>涼度</Text>
+                <Rating defaultValue={review.coolness} color="brick-red.6" size="md" fractions={2} readOnly></Rating>
+              </Group>
+            </Card.Section>
 
-						<Card.Section className={styles.cardSection}>
-							<Text>{review.comment}</Text>
-						</Card.Section>
-					</Card>
-				)
-			})}
-		</Container>
-	)
+            <Card.Section className={styles.cardSection}>
+              <Text>{review.comment}</Text>
+            </Card.Section>
+          </Card>
+        )
+      })}
+    </Container>
+  )
 }
 
 export default CourseReviewsPanel;
