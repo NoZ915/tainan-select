d00928639303fd0938f8072d33af279a17e221f4
NoZ915 <tina0915tw0915@gmail.com>
2025-04-27 12:30:06 +0800

    feat:Add ToggleInterestRequest

--- backend/controllers/interestController.ts
+++ backend/controllers/interestController.ts
@@ -5,10 +5,12 @@ export const toggleInterest: RequestHandler = async(req, res): Promise<void> => 
     try{
         const user_id = req.user?.id;
         const {course_id} = req.body;
-        if(user_id){
-            await InterestService.toggleInterest(user_id, course_id);
+        if(!user_id){
+            res.status(401).json({ message: "未授權的使用者" });
+            return;
         }
-        res.status(200).json("Add interest success!");
+        const result = await InterestService.toggleInterest(user_id, course_id);
+        res.status(200).json(result);
     }catch(err){
         res.status(500).json({ message: err });
     }
--- backend/services/interestService.ts
+++ backend/services/interestService.ts
@@ -2,21 +2,37 @@ import db from "../models";
 import CourseRepository from "../repositories/courseRepository";
 import InterestRepository from "../repositories/interestRepository";
 
+interface ToggleInterestResult{
+	isInterest: boolean,
+	message: string
+}
+
 class InterestService {
-	async toggleInterest(user_id: number, course_id: number): Promise<void> {
+	async toggleInterest(user_id: number, course_id: number): Promise<ToggleInterestResult> {
 		const transaction = await db.sequelize.transaction();
 
 		try {
+			let result: ToggleInterestResult;
 			const existingInterest = await InterestRepository.findInterest(user_id, course_id);
 			if (existingInterest) {
 				await InterestRepository.removeInterest(user_id, course_id, transaction);
 				await CourseRepository.decrementCount(course_id, "interests_count", transaction);
+				result = {
+					isInterest: false,
+					message: "已移除收藏"
+				}
+
 			} else {
 				await InterestRepository.addInterest(user_id, course_id, transaction);
 				await CourseRepository.incrementCount(course_id, "interests_count", transaction);
+				result = {
+					isInterest: true,
+					message: "已加入收藏"
+				}
 			}
 
 			await transaction.commit();
+			return result;
 		} catch (err) {
 			await transaction.rollback();
 			throw err;
